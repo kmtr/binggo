7e981e8b84cb9544f9e8684de06ad469508e444d
Hiroaki Kamei <kmtr@outlook.com>
2015-06-21 13:27:33 +0900

    Fmt applescript

--- binggo.go
+++ binggo.go
@@ -188,14 +188,14 @@ func getWallpaperFile(pictDir string) (PictFiles, error) {
 func changeWallpaper(displayNumber int, pictDir string, f os.FileInfo) error {
 	script := `
 	tell application "System Events"
-	 set desktopCount to count of desktops
-	 repeat with desktopNumber from 1 to desktopCount
-	  tell desktop desktopNumber
-	   if desktopNumber is %d or %d less than 1 then
-		 set picture to "%s/%s"
-		end if
-	  end tell
-	 end repeat
+		set desktopCount to count of desktops
+		repeat with desktopNumber from 1 to desktopCount
+			tell desktop desktopNumber
+				if desktopNumber is %d or %d less than 1 then
+					set picture to "%s/%s"
+				end if
+			end tell
+		end repeat
 	end tell
 	`
 	cmd := exec.Command("osascript", "-e",
